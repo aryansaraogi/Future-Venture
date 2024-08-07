97b09ee9769ef7b3722cfc67a5e5835346347681
Aryan Saraogi <saraogiaryan80@gmail.com>
2024-08-08 00:28:36 +0530

    intialization

---
+++ fv/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponetNavbarComponent } from './componet-navbar/componet-navbar.component';
+import { ComponetFooterComponent } from './componet-footer/componet-footer.component';
+import { ComponetHeaderComponent } from './componet-header/componet-header.component';
+import { ComponetAboutUsComponent } from './componet-about-us/componet-about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponetNavbarComponent,
+    ComponetFooterComponent,
+    ComponetHeaderComponent,
+    ComponetAboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
